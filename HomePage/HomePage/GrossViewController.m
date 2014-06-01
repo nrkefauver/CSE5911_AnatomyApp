53938d4601e7bbfcdb7ec7db1d6abfc80ccb78da
morris <morrispe@mac-cl1160.cse.ohio-state.edu>
2014-06-01 16:45:04 -0400

    Added Gross Anatomy Navigation Bar with connection to the Home page

--- HomePage/HomePage/GrossViewController.m
+++ HomePage/HomePage/GrossViewController.m
@@ -9,7 +9,7 @@
 #import "GrossViewController.h"
 
 @interface GrossViewController ()
-
+@property (nonatomic, strong) NSMutableIndexSet *optionIndices;
 @end
 
 @implementation GrossViewController
@@ -33,6 +33,73 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)onBurger:(id)sender {
+    NSArray *images = @[
+                        [UIImage imageNamed:@"videos"],
+                        [UIImage imageNamed:@"3D Models"],
+                        [UIImage imageNamed:@"Index"],
+                        [UIImage imageNamed:@"g_black copy"],
+                        [UIImage imageNamed:@"home"],
+                        ];
+    //NSArray *colors = @[
+      //                  [UIColor colorWithRed:126/255.f green:242/255.f blue:195/255.f alpha:1],
+        //                [UIColor colorWithRed:126/255.f green:242/255.f blue:195/255.f alpha:1],
+          //              [UIColor colorWithRed:126/255.f green:242/255.f blue:195/255.f alpha:1],
+            //            [UIColor colorWithRed:126/255.f green:242/255.f blue:195/255.f alpha:1],
+              //          [UIColor colorWithRed:126/255.f green:242/255.f blue:195/255.f alpha:1],
+                //        ];
+    
+    //RNFrostedSidebar *callout = [[RNFrostedSidebar alloc] initWithImages:images selectedIndices:self.optionIndices borderColors:colors];
+    RNFrostedSidebar *callout = [[RNFrostedSidebar alloc] initWithImages:images selectedIndices:self.optionIndices];
+    callout.delegate = self;
+    callout.showFromRight = YES;
+    [callout show];
+}
+
+- (void)sidebar:(RNFrostedSidebar *)sidebar didTapItemAtIndex:(NSUInteger)index {
+    NSLog(@"Tapped item at index %i",index);
+    
+    
+    //Videos clicked
+    if (index == 0) {
+        [sidebar dismissAnimated:YES completion:nil];
+        //TODO
+    }
+    
+    //3D Models clicked
+    else if (index == 1) {
+        [sidebar dismissAnimated:YES completion:nil];
+        //TODO
+    }
+    
+    //Index clicked
+    else if (index == 2) {
+        [sidebar dismissAnimated:YES completion:nil];
+        //TODO
+    }
+    
+    //Gross Home clicked
+    else if (index == 3) {
+        // Do nothing
+    }
+    
+    //Home clicked
+    else if (index == 4) {
+        [sidebar dismissAnimated:YES completion:nil];
+        [self performSegueWithIdentifier:@"GrossToHomeSegue" sender:self];
+    }
+    
+}
+
+- (void)sidebar:(RNFrostedSidebar *)sidebar didEnable:(BOOL)itemEnabled itemAtIndex:(NSUInteger)index {
+    if (itemEnabled) {
+        [self.optionIndices addIndex:index];
+    }
+    else {
+        [self.optionIndices removeIndex:index];
+    }
 }
 
 /*
