817facdaced4286b2314987e448416a945f0a22c
morris <morrispe@mac-cl1162.cse.ohio-state.edu>
2014-06-03 14:33:43 -0400

    Removed color animations

--- HomePage/HomePage/GrossViewController.m
+++ HomePage/HomePage/GrossViewController.m
@@ -43,16 +43,16 @@
                         [UIImage imageNamed:@"Letter G"],
                         [UIImage imageNamed:@"Home"],
                         ];
-    NSArray *colors = @[
-                        [UIColor colorWithRed:126/255.f green:242/255.f blue:195/255.f alpha:1],
-                       [UIColor colorWithRed:126/255.f green:242/255.f blue:195/255.f alpha:1],
-                       [UIColor colorWithRed:126/255.f green:242/255.f blue:195/255.f alpha:1],
-                        [UIColor colorWithRed:126/255.f green:242/255.f blue:195/255.f alpha:1],
-                       [UIColor colorWithRed:126/255.f green:242/255.f blue:195/255.f alpha:1],
-                        ];
-    
-    RNFrostedSidebar *callout = [[RNFrostedSidebar alloc] initWithImages:images selectedIndices:self.optionIndices borderColors:colors];
-    //RNFrostedSidebar *callout = [[RNFrostedSidebar alloc] initWithImages:images selectedIndices:self.optionIndices];
+//    NSArray *colors = @[
+//                        [UIColor colorWithRed:126/255.f green:242/255.f blue:195/255.f alpha:1],
+//                        [UIColor colorWithRed:126/255.f green:242/255.f blue:195/255.f alpha:1],
+//                        [UIColor colorWithRed:126/255.f green:242/255.f blue:195/255.f alpha:1],
+//                        [UIColor colorWithRed:126/255.f green:242/255.f blue:195/255.f alpha:1],
+//                        [UIColor colorWithRed:126/255.f green:242/255.f blue:195/255.f alpha:1],
+//                        ];
+//    
+//    RNFrostedSidebar *callout = [[RNFrostedSidebar alloc] initWithImages:images selectedIndices:self.optionIndices borderColors:colors];
+    RNFrostedSidebar *callout = [[RNFrostedSidebar alloc] initWithImages:images selectedIndices:self.optionIndices];
     callout.delegate = self;
     callout.showFromRight = YES;
     [callout show];
