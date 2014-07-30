f1439ad7b3f56de07b1164476d617f7288eb0ada
morris <morrispe@mac-cl1168.cse.ohio-state.edu>
2014-07-30 11:03:17 -0400

    Cleaned HomePageViewController commented out code

--- HomePage/HomePage/HomePageViewController.h
+++ HomePage/HomePage/HomePageViewController.h
@@ -10,6 +10,4 @@
 
 @interface HomePageViewController : UIViewController
 
-//- (void) doAThing;
-
 @end
--- HomePage/HomePage/HomePageViewController.m
+++ HomePage/HomePage/HomePageViewController.m
@@ -17,47 +17,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    
-    //[self addButton];
 }
-
-//-(void) addButton
-//{
-//    // Create a button
-//    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-//    [button addTarget:self
-//               action:@selector(doAThing)
-//     forControlEvents:UIControlEventTouchUpInside];
-//    [button setTitle:@"PROGRAMMATIC ACTION" forState:UIControlStateNormal];
-//    UIImage *img = [UIImage imageNamed:@"home.png"];
-//    [button setImage:img forState:UIControlStateNormal];
-//    button.frame = CGRectMake(20.0, 70.0, 300.0, 40.0);
-//    [self.view addSubview:button];
-//}
-//
-//- (void) doAThing
-//{
-//    [self addButton2];
-//}
-//
-//-(void) addButton2
-//{
-//    // Create a button
-//    UIButton *button2 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-//    [button2 addTarget:self
-//               action:@selector(doAThing2)
-//     forControlEvents:UIControlEventTouchUpInside];
-//    [button2 setTitle:@"Yo Dawg I heard ya liked buttons" forState:UIControlStateNormal];
-//    UIImage *img = [UIImage imageNamed:@"home.png"];
-//    [button2 setImage:img forState:UIControlStateNormal];
-//    button2.frame = CGRectMake(20.0, 300.0, 300.0, 40.0);
-//    [self.view addSubview:button2];
-//}
-//
-//- (void) doAThing2
-//{
-//    self.title = @"This Works Good";
-//}
 
 - (void)didReceiveMemoryWarning
 {
